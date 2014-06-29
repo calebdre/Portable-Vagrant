ecdd6d67b9b6057b5e64d46e1a545ce533018d26
temp <temp@Calebs-MacBook-Air.local>
2014-06-29 10:08:11 -0400

    added php55 dependancy

--- puppet/modules/php55/manifests/init.pp
+++ puppet/modules/php55/manifests/init.pp
@@ -4,15 +4,14 @@ class php55
 	#PHP 5.5 setup
 
 	#https://launchpad.net/~ondrej/+archive/php5
-	exec
-	{
-		'add php55 apt-repo':
+	exec{'add php55 apt-repo':
 			command => '/usr/bin/add-apt-repository ppa:ondrej/php5 -y',
 			require => [Package['python-software-properties']],
 	}
 
 	exec { "php55 apt update":
 		command => 'apt-get update',
+		require => Exec['add php55 apt-repo']
 	}
 
 	exec { "add php5":
