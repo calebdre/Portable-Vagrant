74166e59ca5f0a8e1e9dfff9b313ecee439efeaa
Caleb Lewis <caleb_dre@yahoo.com>
2015-12-12 14:01:56 -0500

    added support for being able to access mysql DB from local machine

--- Vagrantfile
+++ Vagrantfile
@@ -1,7 +1,9 @@
 Vagrant.configure("2") do |config|
+  config.vm.network "private_network", ip: "192.168.100.93"
   config.vm.box = "vagrant-ubuntu-trusty-64"
   config.vm.box_url = "https://cloud-images.ubuntu.com/vagrant/trusty/current/trusty-server-cloudimg-amd64-vagrant-disk1.box"
   config.vm.network :forwarded_port, host: 5000, guest: 80
+  config.vm.network :forwarded_port, host: 3306, guest: 3306
 
   config.vm.provision :puppet do |puppet|
     puppet.manifests_path = 'puppet/manifests'
--- puppet/modules/mysql/manifests/init.pp
+++ puppet/modules/mysql/manifests/init.pp
@@ -34,4 +34,26 @@ class mysql
             command => "/usr/bin/mysql -uroot -p$mysqlPassword -e 'create database `database`;'",
             require => [Service["mysql"], Exec["set-mysql-password"]]
     }
+	
+	file { "my.cnf":
+		ensure => file,
+		path => "/etc/mysql/my.cnf",
+		source => "puppet:///modules/mysql/my.cnf",
+		mode    => 600,
+		owner   => "root",
+		group   => "root",
+		require => Service["mysql"]
+	}
+	
+	exec{
+		"restart mysql":
+		command => "service mysql restart",
+		require => File["my.cnf"]
+	}
+	
+	exec{
+		"update user for host":
+		command => "/usr/bin/mysql -uroot -e 'update mysql.user set host = \"%\" where mysql.user.Host = \"localhost\"; FLUSH PRIVILEGES;'",
+		require => File["my.cnf"]
+	}
 }
